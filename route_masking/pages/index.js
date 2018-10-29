2c4c96c7011c7f281535ec26f9ef5a28eb361552
Christian Burkhart <burkhart.christian@gmail.com>
2018-10-29 21:59:38 +0100

    feat: started route masking app

---
+++ route_masking/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Index = (props) => (
+  <div>
+    <Head>
+        <title>Meine Index Seite</title>
+    </Head>
+    <h1>This is my index page</h1>
+  </div>
+)
+
+export default Index;
---
+++ route_masking/pages/test.js
@@ -0,0 +1,7 @@
+const Index = (props) => (
+  <div>
+    <h1>Mein Test</h1>
+  </div>
+)
+
+export default Index;
